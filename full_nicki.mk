e3efed88e409dae71aca32a7722909c5172a8e87
Arnav Gosain <nolinuxnoparty@gmail.com>
2014-08-08 13:55:27 -0400

    Eos

--- full_nicki.mk
+++ full_nicki.mk
@@ -1,3 +1,28 @@
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This file is the build configuration for a full Android
+# build for maguro hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps). Except for a few implementation
+# details, it only fundamentally contains two inherit-product
+# lines, full and maguro, hence its name.
+#
+
+# inherit from the EOS vendor, if present
+$(call inherit-product-if-exists, vendor/eos/config/common_full_phone.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/sony/nicki/nicki.mk)
 
@@ -19,3 +44,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Release name
 PRODUCT_RELEASE_NAME := XperiaM
+
+# Copy Bootanimation
+PRODUCT_COPY_FILES += \
+   vendor/eos/prebuilt/common/bootanimation/720.zip:system/media/bootanimation.zip
